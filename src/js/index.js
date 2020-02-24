bb1dee4290e4ef4893fa9a95c13a11fe9d061a61
Alex <alexolthoff@tutanota.com>
2020-02-25 00:37:09 +0800

    Finished search controller and added state

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
 import Search from './models/Search'
 
-const search = new Search('pizza');
-console.log(search)
+/*
+Search object
+Current recipe object
+shopping list object
+linked recipes
+*/
+const state = {
 
-search.getResults();
+}
+document.querySelector('.search').addEventListener('submit', (e) => {
+  e.preventDefault()
+  controlSearch()
+})
+const controlSearch = async () => {
+  // Get search query
+  const query = "pizza"
+
+  if (query) {
+    // create search object
+    state.search = new Search(query)
+  }
+  // Prepare UI for what will happen
+
+  //search of recipes
+  await state.search.getResults()
+
+  //Display results on UI
+  console.log(state.search.result)
+}
