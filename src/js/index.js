7d29f4b79a5f5f211de8e7e332a8b110233a0079
Alex <alexolthoff@tutanota.com>
2020-02-24 23:16:01 +0800

    Added search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,6 @@
-import axios from "axios";
-async function getResults(query) {
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults("pizza");
+import Search from './models/Search'
+
+const search = new Search('pizza');
+console.log(search)
+
+search.getResults();
