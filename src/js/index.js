d2a93a2ee3c136e8781d4147c2fe3eac666b13cb
Alex <alexolthoff@tutanota.com>
2020-02-26 01:34:38 +0800

    added basic search and display functionality to searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import Search from './models/Search'
-
+import * as SearchView from './views/searchView'
+import {
+  elements
+} from './views/base'
 /*
 Search object
 Current recipe object
@@ -9,23 +12,24 @@ linked recipes
 const state = {
 
 }
-document.querySelector('.search').addEventListener('submit', (e) => {
+elements.searchform.addEventListener('submit', (e) => {
   e.preventDefault()
   controlSearch()
 })
 const controlSearch = async () => {
   // Get search query
-  const query = "pizza"
+  const query = SearchView.getInput()
 
   if (query) {
     // create search object
     state.search = new Search(query)
   }
   // Prepare UI for what will happen
-
+  SearchView.clearInput()
+  SearchView.clearResults()
   //search of recipes
   await state.search.getResults()
 
   //Display results on UI
-  console.log(state.search.result)
+  SearchView.renderResults(state.search.result)
 }
---
+++ src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    searchInput: document.querySelector('.search__field'),
+    searchform: document.querySelector('.search'),
+    searchResultsList: document.querySelector('.results__list')
+}
