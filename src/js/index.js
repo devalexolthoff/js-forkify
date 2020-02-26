0f6597693cd4e765b511914deae7022737d3933c
Alex <alexolthoff@tutanota.com>
2020-02-26 12:28:15 +0800

    Added a spinner to the page for the loading process. Work done in searchView and base

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-import Search from './models/Search'
-import * as SearchView from './views/searchView'
+import Search from './models/Search';
+import * as SearchView from './views/searchView';
 import {
-  elements
-} from './views/base'
+  elements,
+  renderLoader,
+  clearLoader
+} from './views/base';
 /*
 Search object
 Current recipe object
@@ -13,23 +15,24 @@ const state = {
 
 }
 elements.searchform.addEventListener('submit', (e) => {
-  e.preventDefault()
-  controlSearch()
+  e.preventDefault();
+  controlSearch();
 })
 const controlSearch = async () => {
   // Get search query
-  const query = SearchView.getInput()
+  const query = SearchView.getInput();
 
   if (query) {
     // create search object
     state.search = new Search(query)
   }
   // Prepare UI for what will happen
-  SearchView.clearInput()
-  SearchView.clearResults()
+  SearchView.clearInput();
+  SearchView.clearResults();
+  renderLoader(elements.loaderSpace);
   //search of recipes
-  await state.search.getResults()
-
+  await state.search.getResults();
   //Display results on UI
-  SearchView.renderResults(state.search.result)
+  clearLoader()
+  SearchView.renderResults(state.search.result);
 }
